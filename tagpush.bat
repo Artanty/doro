89584cef0ac06b2cc6f2dc5ab1e663b6c48c36f5
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-04-21 01:33:43 +0300

    * build: +current version tag check, add commit nodies to tag annotation;

--- tagpush.bat
+++ tagpush.bat
@@ -16,6 +16,15 @@ for /f "delims=" %%i in ('git diff origin/master..HEAD') do (
 :: Change to the 'back' directory
 cd back
 
+:: Get the current version from package.json
+for /f "delims=" %%i in ('node -p "require('./package.json').version"') do set CURRENT_VERSION=%%i
+
+:: Check if the current version has a tag
+git rev-parse v%CURRENT_VERSION% > nul 2>&1 || (
+    echo There is no tag for the current version v%CURRENT_VERSION%. Create a tag for the current version before running this script.
+    exit /b
+)
+
 :: Increment the patch version and save the new value in a variable
 for /f "delims=" %%i in ('npm version patch --no-git-tag-version') do (
     set NEW_VERSION=%%i
@@ -29,8 +38,15 @@ git add .
 git commit -m "Bump version to %TAG_VERSION%"
 git push origin master
 
-:: Create an annotated tag with the new version
-git tag -a "v%TAG_VERSION%" -m "Version %TAG_VERSION%"
+:: Get the commit messages since the last tag
+for /f "delims=" %%i in ('git log --pretty^=format:"%%s" HEAD...v%CURRENT_VERSION%') do (
+    set COMMIT_MESSAGES=%%i
+)
+
+:: Create an annotated tag with the new version and commit messages
+git tag -a "v%TAG_VERSION%" -m "Version %TAG_VERSION%
+Commit Messages:
+%COMMIT_MESSAGES%"
 
 :: Push the new tag to the remote repository
 git push origin "v%TAG_VERSION%"
