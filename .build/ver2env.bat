e0283732cd063adf6f8a4524138dc254fadcd745
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-05-17 13:36:50 +0300

    * build: DORO-0002  build fix: ver2env https://red-kqjc.onrender.com/plan/get-ext-task/DORO-0002

--- .build/ver2env.bat
+++ .build/ver2env.bat
@@ -13,16 +13,25 @@ if not exist "%ENV_FILE%" (
     exit /b
 )
 
+:: Initialize a flag to track if the CURRENT_VERSION variable was found
+set VAR_FOUND=false
+
 :: Read the .env file and replace the CURRENT_VERSION line
 (
     for /f "tokens=1* delims==" %%a in (%ENV_FILE%) do (
         if /i "%%a"=="CURRENT_VERSION" (
             echo CURRENT_VERSION=%CURRENT_VERSION%
+            set VAR_FOUND=true
         ) else (
             echo %%a=%%b
         )
     )
 ) > temp.env
+
+:: If the CURRENT_VERSION variable was not found, append it to the end of the file
+if "%VAR_FOUND%"=="false" (
+    echo CURRENT_VERSION=%CURRENT_VERSION% >> temp.env
+)
 
 :: Move the temp file to the original .env file
 move /y temp.env %ENV_FILE% > nul
