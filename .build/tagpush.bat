6715a0bb850ea964e3529f044700ed2ba88134fe
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-05-17 19:12:34 +0300

    * build: DORO-0002  build fix: tagpush try https://red-kqjc.onrender.com/plan/get-ext-task/DORO-0002

--- .build/tagpush.bat
+++ .build/tagpush.bat
@@ -22,9 +22,6 @@ for /f "tokens=1,2,3 delims=." %%a in ("%VERSION_NUMBER%") do (
     set NEW_VERSION=%%a.%%b.!NEW_PATCH!
 )
 
-:: Create a new tag with the incremented version number
-git tag -a "v!NEW_VERSION!" -m "App version: !NEW_VERSION!"
-
 :: Get the commit messages between the latest tag and the previous one
 for /f "delims=" %%i in ('git log --pretty^=format:"%%s" !LATEST_TAG!..HEAD') do (
     set "COMMIT_MESSAGES=%%i"
@@ -33,5 +30,11 @@ for /f "delims=" %%i in ('git log --pretty^=format:"%%s" !LATEST_TAG!..HEAD') do
 :: Display the new version and commit messages
 echo New version: !NEW_VERSION!
 echo Commit messages: !COMMIT_MESSAGES!
+
+:: Create an annotated tag with the new version and commit messages, including web & back version
+git tag -a "v!NEW_VERSION!" -m "App version: !NEW_VERSION!" -m "Changes: !COMMIT_MESSAGES!"
+
+:: Push the new tag to the remote repository
+git push origin "v!NEW_VERSION!"
 
 endlocal
